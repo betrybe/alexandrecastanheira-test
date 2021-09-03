2ba0046a182c5f05aa01223ad21685890bb0e272
Alexandre Castanheira <alexandre.jcastanheira@hsl.org.br>
2021-09-02 22:21:22 -0300

    Criado helper para acesso mongodb

--- src/models/mongodb_helper.js
+++ src/models/mongodb_helper.js
@@ -1,20 +1,59 @@
+const { MongoClient } = require('mongodb');
+
 const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
-//const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 // const DB_NAME = 'Cookmaster';
 
 module.exports = {
-    insert() {
-      console.log('inserindo no mongodb');
+    insert(collection, data) {
+        console.log('inserindo no mongodb');
+        MongoClient.connect(MONGO_DB_URL, (err, db) => {
+            if (err) throw err;
+            const dbo = db.db(DB_NAME);
+            dbo.collection(collection).insertOne(data, (error, res) => {
+                if (error) throw error;
+                console.log('1 document inserted');
+                db.close();
+            });
+        });
     },
-    update() {
+    update(collection, data, query) {
         console.log('Atualizando no mongodb');
+        MongoClient.connect(MONGO_DB_URL, (err, db) => {
+            if (err) throw err;
+            const dbo = db.db(DB_NAME);
+
+            dbo.collection(collection).updateOne(query, data, (error, res) => {
+                if (error) throw error;
+                console.log('1 document updated');
+                db.close();
+            });
+        });
     },
-    list() {
+    list(collection, query) {
         console.log('Listando no mongodb');
+        MongoClient.connect(MONGO_DB_URL, (err, db) => {
+            if (err) throw err;
+            const dbo = db.db(DB_NAME);
+            dbo.collection(collection).find(query).toArray((error, res) => {
+                if (error) throw error;
+                console.log(res);
+                db.close();
+            });
+        });
     },
-    get() {
+    get(collection, query) {
         console.log('Realizando get no mongodb');
-    }
+        MongoClient.connect(MONGO_DB_URL, (err, db) => {
+            if (err) throw err;
+            const dbo = db.db(DB_NAME);
+            dbo.collection(collection).findOne(query, (error, res) => {
+                if (error) throw error;
+                console.log(res.name);
+                db.close();
+            });
+        });
+    },
 };
