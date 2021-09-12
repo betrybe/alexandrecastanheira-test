884850a0a30b01d8c35c984c426252902c83472e
Alexandre <03.alexandre@gmail.com>
2021-09-12 18:30:14 -0300

    Criado rotas e serviços

---
+++ src/api/services/crud_service.js
@@ -0,0 +1,125 @@
+/**
+ * Service responsável por requisições CRUD
+ */
+const Service = require('./service');
+const ValidateService = require('../../helpers/validate_helper');
+
+class CrudService extends Service {
+    constructor(request, model) {
+        super();
+        this.request = request;
+        this.query = request.query;
+        this.body = request.body;
+        this.params = request.params;
+        this.model = model;
+
+        this.validate = new ValidateService(this.model, this.query);
+    }
+
+    /**
+     * Realiza um insert na model vinculada ao service.
+     *
+     * @param {Array} values
+     * @returns Object
+     */
+    async insert() {
+        let result = '';
+        try {
+            result = await this.model.insert(this.body);
+            this.message = JSON.stringify({ [this.model.entitySingular]: result });
+            this.status = 201;
+        } catch (error) {
+            this.message = JSON.stringify({ message: error });
+            this.status = 500;
+        }
+        return result;
+    }
+
+    /**
+     * Realiza um update na model vinculada ao service.
+     *
+     * @param {Array} values
+     * @param {Array} query
+     * @returns Object
+     */
+    async update() {
+        let result = '';
+        try {
+            result = await this.model.update(this.body, this.params.id);
+            this.message = JSON.stringify(result);
+            this.status = 200;
+        } catch (error) {
+            this.message = JSON.stringify({ message: error });
+            this.status = 500;
+        }
+
+        return result;
+    }
+
+    /**
+     * Realiza a listagem da model vinculada ao service.
+     *
+     * @param {Array} query
+     * @returns Array
+     */
+    async list(query) {
+        let result = '';
+        try {
+            result = await this.model.list(query);
+        } catch (error) {
+            this.message = JSON.stringify({ message: error });
+            this.status = 500;
+        }
+
+        return result;
+    }
+
+    /**
+     * Obtem um registro da model vinculada ao service.
+     *
+     * @param {Array} query
+     * @returns Object
+     */
+    async get() {
+        let result = '';
+        try {
+            result = await this.model.get(this.params.id);
+            if (!result) {
+                this.message = JSON.stringify({ message: `${this.model.entitySingular} not found` });
+                this.status = 404;
+                return result;
+            }
+
+            this.message = JSON.stringify(result);
+            this.status = 200;
+
+        } catch (error) {
+            this.message = JSON.stringify({ message: error });
+            this.status = 500;
+        }
+
+        return result;
+    }
+
+    /**
+     * Remove um registro da model vinculada ao service.
+     *
+     * @param {Array} query
+     * @returns Object
+     */
+    async remove() {
+        let result = '';
+        try {
+            result = await this.model.remove(this.params.id);
+            this.message = JSON.stringify({ [this.model.entitySingular]: result });
+            this.status = 204;
+        } catch (error) {
+            this.message = JSON.stringify({ message: error });
+            this.status = 500;
+        }
+
+        return result;
+    }
+}
+
+module.exports = CrudService;
---
+++ src/api/services/user_service.js
@@ -0,0 +1,119 @@
+/**
+ * Service responsável para tratar as requisições da entidade de usuários.
+ */
+const CrudService = require('./crud_service');
+const ValidateService = require('../../helpers/validate_helper');
+const UserModel = require('../models/user');
+
+const model = new UserModel();
+
+class UserService extends CrudService {
+    /**
+     * Construtor do service.
+     *
+     * @param {Object} request
+     */
+    constructor(request) {
+        super(request, model);
+    }
+
+    /**
+     * Middleware para tratamento dos campos durante o insert.
+     *
+     * @param {Object} request
+     * @param {Object} res
+     * @param {Object} next
+     *
+     * @returns void
+     */
+    static validateUserInsert(request, response, next) {
+        try {
+            if (!request.body.email || !request.body.name || !request.body.password) {
+                return response.status(400).send({ message: 'Invalid entries. Try again.' });
+            }
+        } catch (error) {
+            return response.status(400).send({ message: 'Invalid entries. Try again.' });
+        }
+
+        next();
+    }
+
+    static validateEmail(request, response, next) {
+        const email = request.body.email || '';
+        if (!ValidateService.emailValid(email)) {
+            return response.status(400).send({ message: 'Invalid entries. Try again.' });
+        }
+
+        next();
+    }
+
+    /**
+     * Middleware para validar se um determinado campo é único.
+     * @param {Object} request
+     * @param {Object} response
+     * @param {Object} next
+     */
+    static validateUnique(request, response, next) {
+        const service = new UserService(request);
+        const uniqueField = 'email';
+        const uniqueValue = request.body[uniqueField];
+        service.validate.unique(uniqueField, uniqueValue).then((isUnique) => {
+            if (!isUnique) {
+                return response.status(409).send({ message: 'Email already registered' });
+            }
+
+            next();
+        });
+    }
+
+    /**
+     * Rota para inserção de registros.
+     *
+     * @param {Object} request
+     * @param {Object} response
+     */
+     static insertRoute(request, response) {
+        const service = new UserService(request);
+        const defaultRole = 'user';
+        service.insert(defaultRole).then((_val) => {
+            service.sendResponse(response);
+        });
+    }
+
+    /**
+     * Rota para inserção de registros.
+     *
+     * @param {Object} request
+     * @param {Object} response
+     */
+     static insertAdminRoute(request, response) {
+        const service = new UserService(request);
+        const defaultRole = 'admin';
+        service.insert(defaultRole).then((_val) => {
+            service.sendResponse(response);
+        });
+    }
+
+    /**
+     * Processamento da operação de inserção.
+     *
+     * @returns boolean
+     */
+    async insert(defaultRole) {
+        const isUnique = await this.validate.unique('email', this.body.email);
+        if (!isUnique) {
+            this.message = JSON.stringify({ message: 'Email already registered' });
+            this.status = 409;
+            return false;
+        }
+
+        this.body.role = defaultRole;
+        const values = this.body;
+        const result = await super.insert(values);
+        this.message = JSON.stringify({ user: result });
+        this.status = 201;
+        return result;
+    }
+}
+
+module.exports = UserService;
